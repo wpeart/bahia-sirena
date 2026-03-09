c9987b1d1c1f323cfd40aa531fbf60184764a23c
Wilbert Peart <wilbert.peart@techcocreators.com>
2026-03-09 17:35:20 -0600

    Add ESLint directive to suppress warning for useLanguage export

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -143,6 +143,7 @@ export const LanguageProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useLanguage = () => {
   const context = useContext(LanguageContext);
   if (!context) {
